b35142d08e3f88b59f85c5a23e9f4f88d89856d3
SmokedKoala <k.azarov@inbox.ru>
2023-02-25 20:50:45 +0300

    upload photo to back

--- backend/src/main/kotlin/ru/mirea/backend/controllers/UploadController.kt
+++ backend/src/main/kotlin/ru/mirea/backend/controllers/UploadController.kt
@@ -1,5 +1,6 @@
 package ru.mirea.backend.controllers
 
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -8,6 +9,7 @@ import org.springframework.web.multipart.MultipartFile
 @RestController
 class UploadController {
 
+    @CrossOrigin
     @PostMapping("/upload")
     fun uploadImage(@RequestParam("photo") photo: MultipartFile):String {
         return photo.bytes.toString()

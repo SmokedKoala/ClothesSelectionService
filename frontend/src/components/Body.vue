527b3c192c78ba67d6931e7169feefe8e2a5065d
SmokedKoala <k.azarov@inbox.ru>
2023-03-11 17:56:05 +0300

    add preview segmentation and uploaded photo info

--- frontend/src/components/Body.vue
+++ frontend/src/components/Body.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
 
+    <!--    до загрузки фото-->
     <div class="image-upload" v-if="!selectedFile">
       <label for="file-input">
         <img src="../assets/plus.png"/>
@@ -8,8 +9,30 @@
       <input id="file-input" type="file" accept="image/*" @change="onFileSelected">
     </div>
 
+    <!-- после загрузки фото-->
     <div v-if="preview">
-      <img :src="preview" class="img-preview">
+
+      <!--      блок с загруженным изображением-->
+      <div class="block-left">
+        <img :src="preview" class="img-preview">
+      </div>
+
+      <!--      блок с информацией по фото и комплекту-->
+      <div class="block-right">
+
+        <!--        блок с информацией по фото-->
+        <div class="block-uploaded-info">
+          <h3 style="display:inline;">основные цвета: </h3>
+          <div id="selected-color" :style="{'background-color':clothInfo.color}"/>
+          <h3>тип одежды: {{clothInfo.type}}</h3>
+        </div>
+
+        <!--        блок с информацией по комплекту-->
+        <div class="block-offer-info">
+
+        </div>
+      </div>
+
       <br>
       <button @click="onFileUpload">Начать анализ</button>
     </div>
@@ -28,8 +51,43 @@
 }
 
 .img-preview {
-  max-height: 400px;
-  max-width: 200px;
+  min-height: 400px;
+  min-width: 200px;
+
+}
+
+.block-left {
+  width: 50%;
+  height: 400px;
+  overflow: auto;
+  float: left;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.block-right {
+  width: 50%;
+  height: 400px;
+  overflow: auto;
+}
+
+.block-uploaded-info {
+  height: 50%;
+}
+
+#selected-color {
+  height: 50px;
+  width: 50px;
+  outline: 2px solid #000;
+  display:inline-block;
+
+}
+
+.block-offer-info {
+  height: 50%;
+  background-color: #56ec2a;
+  overflow: auto;
 }
 </style>
 
@@ -42,6 +100,7 @@ export default {
     return {
       selectedFile: null,
       preview: null,
+      clothInfo: null,
     }
   },
   methods: {
@@ -52,9 +111,11 @@ export default {
         reader.onload = (e) => {
           this.preview = e.target.result;
         }
-        this.selectedFile=input.files[0];
+        this.selectedFile = input.files[0];
         reader.readAsDataURL(input.files[0]);
       }
+      //TODO заменить на вызов бэка
+      this.clothInfo = {type: "футболка", color: ["#ff8300"]}
     },
     onFileUpload() {
       const formData = new FormData

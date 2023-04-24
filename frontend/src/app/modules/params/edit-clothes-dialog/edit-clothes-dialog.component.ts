d977f73c5e95de9d6f84ab035ad4fd8023c62496
SmokedKoala <k.azarov@inbox.ru>
2023-04-24 21:16:55 +0300

    Front: refactor possible clothes select values

--- frontend/src/app/modules/params/edit-clothes-dialog/edit-clothes-dialog.component.ts
+++ frontend/src/app/modules/params/edit-clothes-dialog/edit-clothes-dialog.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
 import { TuiDialogContext } from '@taiga-ui/core';
 import { POLYMORPHEUS_CONTEXT } from '@tinkoff/ng-polymorpheus';
 import { FormControl, FormGroup } from '@angular/forms';
-import { map, Observable } from 'rxjs';
+import { bottomValues, footwearValues } from '@shared/clothes-select-values';
 
 
 @Component({
@@ -17,30 +17,8 @@ export class EditClothesDialogComponent {
     secondClothes: new FormControl(this.data.secondSuggestedClothes),
   });
 
-  firstClothesImageSrc$: Observable<string> = this.form.get('firstClothes')!.valueChanges.pipe(
-    map(clothes => clothes.imageSrc),
-  );
-
-  readonly firstClothes = [
-    {name: 'Кеды', value: 'keds', imageSrc: 'assets/images/footwear/keds.jpg'},
-    {name: 'Кроссовки', value: 'sneakers', imageSrc: 'assets/images/footwear/sneakers.jpg'},
-    {name: 'Ботинки', value: 'boots', imageSrc: 'assets/images/footwear/boots.jpg'},
-    {name: 'Туфли', value: 'shoes', imageSrc: 'assets/images/footwear/shoes.jpg'},
-    {name: 'Сапоги', value: 'boots', imageSrc: 'assets/images/footwear/high-boots.jpg'},
-    {name: 'Тапочки', value: 'slippers', imageSrc: 'assets/images/footwear/slippers.avif'},
-    {name: 'Сандали', value: 'sandals', imageSrc: 'assets/images/footwear/sandals.jpg'},
-    {name: 'Босоножки', value: 'pumps', imageSrc: 'assets/images/footwear/pumps.jpg'},
-    {name: 'Подкрадули', value: 'podkraduli', imageSrc: 'assets/images/footwear/podkraduli.jpg'},
-    {name: 'Бархатные тяги', value: 'barkh_tyagi', imageSrc: 'assets/images/footwear/barkh_tyagi.jpg'},
-  ];
-
-  readonly secondClothes = [
-    {name: 'Шорты', value: 'shorts', imageSrc: 'assets/images/bottoms/shorts.jpg'},
-    {name: 'Брюки', value: 'trousers', imageSrc: 'assets/images/bottoms/trousers.jpg'},
-    {name: 'Юбка', value: 'skirt', imageSrc: 'assets/images/bottoms/skirt.jpg'},
-    {name: 'Джинсы', value: 'jeans', imageSrc: 'assets/images/bottoms/jeans.avif'},
-    {name: 'Леггинсы', value: 'leggings', imageSrc: 'assets/images/bottoms/leggings.avif'},
-  ];
+  readonly firstClothesSelectItems = footwearValues;
+  readonly secondClothesSelectItems = bottomValues;
 
   get data(): any {
     return this.context.data;
---
+++ frontend/src/app/shared/clothes-select-values.ts
@@ -0,0 +1,20 @@
+export const footwearValues = [
+  {name: 'Кеды', value: 'keds', imageSrc: 'assets/images/footwear/keds.jpg'},
+  {name: 'Кроссовки', value: 'sneakers', imageSrc: 'assets/images/footwear/sneakers.jpg'},
+  {name: 'Ботинки', value: 'boots', imageSrc: 'assets/images/footwear/boots.jpg'},
+  {name: 'Туфли', value: 'shoes', imageSrc: 'assets/images/footwear/shoes.jpg'},
+  {name: 'Сапоги', value: 'boots', imageSrc: 'assets/images/footwear/high-boots.jpg'},
+  {name: 'Тапочки', value: 'slippers', imageSrc: 'assets/images/footwear/slippers.avif'},
+  {name: 'Сандали', value: 'sandals', imageSrc: 'assets/images/footwear/sandals.jpg'},
+  {name: 'Босоножки', value: 'pumps', imageSrc: 'assets/images/footwear/pumps.jpg'},
+  {name: 'Подкрадули', value: 'podkraduli', imageSrc: 'assets/images/footwear/podkraduli.jpg'},
+  {name: 'Бархатные тяги', value: 'barkh_tyagi', imageSrc: 'assets/images/footwear/barkh_tyagi.jpg'},
+];
+
+export const bottomValues = [
+  {name: 'Шорты', value: 'shorts', imageSrc: 'assets/images/bottoms/shorts.jpg'},
+  {name: 'Брюки', value: 'trousers', imageSrc: 'assets/images/bottoms/trousers.jpg'},
+  {name: 'Юбка', value: 'skirt', imageSrc: 'assets/images/bottoms/skirt.jpg'},
+  {name: 'Джинсы', value: 'jeans', imageSrc: 'assets/images/bottoms/jeans.avif'},
+  {name: 'Леггинсы', value: 'leggings', imageSrc: 'assets/images/bottoms/leggings.avif'},
+];
